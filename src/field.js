c91215a7250de3c13368591bf51c7923736312ec
PintoPirate <189064953+PintoPirate@users.noreply.github.com>
2025-07-31 19:50:32 -0400

    identify user plots

--- src/field.js
+++ src/field.js
@@ -1,6 +1,8 @@
 const EVM = require("./data/EVM");
 const { batchEventsQuery } = require("./util/BatchEvents");
 const { getCachedOrCalculate } = require("./util/Cache");
+const Concurrent = require("./util/Concurrent");
+const { SNAPSHOT_BLOCK_ARB } = require("./util/Constants");
 
 const getArbWallets = async () => {
   const {
@@ -35,12 +37,76 @@ const getEthWallets = async (evm) => {
   // Start with list of all contract wallets from eth, then filter out those which have migrated
 };
 
+const getArbPods = async (arbWallets) => {
+  const {
+    beanstalk: { contract: beanstalk, storage: bs },
+  } = await EVM.getArbitrum();
+
+  const harvestableIndex = BigInt(
+    await beanstalk.harvestableIndex(0n, { blockTag: SNAPSHOT_BLOCK_ARB })
+  );
+  console.log(`Using Harvestable Index: ${harvestableIndex}`);
+
+  const results = {};
+
+  // Get all pods for the given wallets
+  const TAG = Concurrent.tag("getArbPods-wallet");
+  for (const account of arbWallets) {
+    results[account] = {};
+    await Concurrent.run(TAG, 5, async () => {
+      const plotIndexes = await bs.s.accts[account].fields[0n].plotIndexes;
+      const TAG2 = Concurrent.tag(`getArbPods-plots-${account}`);
+      for (const plotIndex of plotIndexes) {
+        await Concurrent.run(TAG2, 10, async () => {
+          let podCount = await bs.s.accts[account].fields[0n].plots[plotIndex];
+          if (podCount > 0n) {
+            // Shift recorded plot indices by the harvestable index
+            let adjustedIndex = plotIndex - harvestableIndex;
+            if (adjustedIndex < 0n) {
+              // This plot had partially harvested; so we remove those harvestable pods from the recorded pod count
+              podCount += adjustedIndex;
+              adjustedIndex = 0n;
+              console.log(
+                `Removing ${-adjustedIndex} pods from a harvestable plot.`
+              );
+            }
+            if (podCount > 0n) {
+              (results[account] ??= {})[adjustedIndex] = podCount;
+            }
+          } else {
+            // This should never occur
+            console.warn(`Plot ${plotIndex} has no pods for ${account}.`);
+          }
+        });
+      }
+      await Concurrent.allResolved(TAG2);
+    });
+  }
+  await Concurrent.allResolved(TAG);
+
+  return results;
+};
+
 (async () => {
-  const arbWallets = await getCachedOrCalculate(
-    "field-arb-wallets",
-    getArbWallets
+  const arbWallets = await getCachedOrCalculate("field-arb-wallets", () =>
+    getArbWallets()
   );
   console.log(`Proceeding with ${arbWallets.length} Arb wallets.`);
+
+  const arbPods = await getCachedOrCalculate("field-arb-pods", () =>
+    getArbPods(arbWallets)
+  );
+
+  let totalPods = 0n;
+  for (const wallet in arbPods) {
+    for (const plot in arbPods[wallet]) {
+      totalPods += BigInt(arbPods[wallet][plot]);
+    }
+  }
+
+  console.log(
+    `Found ${Number(totalPods) / Math.pow(10, 6)} Pods across ${Object.keys(arbPods).length} wallets.`
+  );
 })();
 
 const pods = {
